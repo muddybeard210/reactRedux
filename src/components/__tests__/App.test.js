d6788f2a5cc250e34d9114281e6ba9e3599a81ce
muddybeard210 <mattsmith203@hotmail.com>
2018-06-18 21:14:04 -0700

    second passing test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,9 +2,14 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 // import CommentBox from "../CommentBox";
 
 it("shows a comment box", () => {
   const component = shallow(<App />);
   expect(component.find(CommentBox).length).toEqual(1);
+});
+it("shows a comment list", () => {
+  const component = shallow(<App />);
+  expect(component.find(CommentList).length).toEqual(1);
 });
